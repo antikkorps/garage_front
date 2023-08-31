bda8e23864bc243c1513325f165d03a07e6aec10
Franck <franckvienot7@gmail.com>
2023-08-31 22:56:01 +0200

    added RouterLink to Menu

--- src/components/NavbarMain.vue
+++ src/components/NavbarMain.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { RouterLink } from 'vue-router'
 import { ref } from 'vue'
 
 const isMenuVisible = ref(false)
@@ -11,12 +12,12 @@ const toggleMenu = () => {
 <template>
   <nav class="bg-white border-gray-200 dark:bg-gray-900">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-      <a href="https://flowbite.com/" class="flex items-center">
+      <RouterLink to="/" class="flex items-center">
         <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
           >Garage Parrot</span
         >
-      </a>
+      </RouterLink>
       <div class="flex md:order-2">
         <button
           type="button"
@@ -59,18 +60,18 @@ const toggleMenu = () => {
           class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
         >
           <li>
-            <a
-              href="#"
+            <RouterLink
+              to="/"
               class="block py-2 pl-3 pr-4 text-white bg-red-700 rounded md:bg-transparent md:text-red-700 md:p-0 md:dark:text-red-500"
               aria-current="page"
-              >Accueil</a
+              >Accueil</RouterLink
             >
           </li>
           <li>
-            <a
-              href="#"
+            <RouterLink
+              to="/about"
               class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-700 md:p-0 md:dark:hover:text-red-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >A Propos</a
+              >A Propos</RouterLink
             >
           </li>
           <li>
