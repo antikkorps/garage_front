0d36cd56feb8589fe9ebb9fe47576fcfa806d2b0
Franck <franckvienot7@gmail.com>
2023-09-04 23:03:15 +0200

    create SearchBar component && start implementing logic

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import HeroSection from '@/components/HeroSection.vue'
 import OurServices from '@/components/OurServices.vue'
+import SearchBar from '@/components/SearchBar.vue'
 </script>
 
 <template>
   <main>
     <HeroSection />
     <OurServices />
+    <SearchBar />
   </main>
 </template>
