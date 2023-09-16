70220dba5795e3c36c6835231a33a97372b07ea1
Franck <franckvienot7@gmail.com>
2023-09-16 19:02:21 +0200

    added the AnnoncesView for displaying all Annonces + created an CardAnnonceDisplayed to create a reusable component

---
+++ src/components/CardAnnonceDisplayed.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const { annonce } = defineProps(['annonce'])
+</script>
+<template>
+  <div
+    class="img_container mx-3 mt-6 flex flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-gray-900 sm:shrink-0 sm:grow sm:basis-0"
+  >
+    <RouterLink to="/services">
+      <img
+        class="rounded-t-lg"
+        src="https://tecdn.b-cdn.net/img/new/standard/city/041.webp"
+        alt="Hollywood Sign on The Hill"
+      />
+    </RouterLink>
+    <div class="p-6">
+      <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+        {{ annonce.title }}
+      </h5>
+      <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+        {{ annonce.description }}
+      </p>
+      <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">{{ annonce.price }} €</p>
+      <RouterLink to="/annonces/{{ annonce.id }}">
+        <button
+          class="mx-auto mt-4 w-auto transform rounded-md bg-red-600 px-5 py-2 text-sm font-medium text-white transition-colors duration-300 hover:bg-red-500 focus:bg-red-600 focus:outline-none lg:w-auto"
+        >
+          Voir l'annonce
+        </button>
+      </RouterLink>
+    </div>
+  </div>
+</template>
+<style scoped>
+.img_container {
+  overflow: hidden;
+}
+img:hover {
+  scale: 1.2;
+  overflow: hidden;
+  translate: 0% -10%;
+  transition: all 0.2s ease-in-out;
+}
+img {
+  transition: all 0.2s ease-in-out;
+}
+</style>
--- src/views/AnnoncesView.vue
+++ src/views/AnnoncesView.vue
@@ -1,6 +1,42 @@
 <script setup lang="ts">
 import NavbarMain from '@/components/NavbarMain.vue'
 import FooterSection from '@/components/FooterSection.vue'
+import CardAnnonceDisplayed from '@/components/CardAnnonceDisplayed.vue'
+import { ref, onMounted } from 'vue'
+import { state } from '@/stores/state'
+import axios from 'axios'
+import apiConfig from '@/config/apiConfig'
+
+const annonces = ref<Annonce[]>([])
+
+const baseUrl = apiConfig.production.baseUrl
+const endpoint = apiConfig.production.endpoints.annoncesAll
+const annoncesQuery = `${baseUrl}${endpoint}`
+
+interface Annonce {
+  id: number
+  title: string
+  description: string
+  price: number
+  kilometrage: number
+  yearofcirculation: number
+  image: string
+  published: boolean
+  author: object
+}
+
+const getAllAnnonces = async () => {
+  try {
+    const response = await axios.get(annoncesQuery)
+    annonces.value = response.data
+  } catch (error) {
+    console.error('Erreur lors de la récupération des annonces :', error)
+  }
+}
+
+onMounted(() => {
+  getAllAnnonces()
+})
 
 const currentPage = 'annonces'
 </script>
@@ -8,6 +44,11 @@ const currentPage = 'annonces'
   <NavbarMain :current-page="currentPage" />
   <div>
     <h1 class="my-20 text-2xl text-center">Here will be displayed all the annonces</h1>
+    <div class="flex justify-center my-10">
+      <div class="grid-cols-1 sm:grid md:grid-cols-4 w-full sm:w-3/4 place-content-evenly">
+        <CardAnnonceDisplayed v-for="annonce in annonces" :key="annonce.id" :annonce="annonce" />
+      </div>
+    </div>
   </div>
   <FooterSection />
 </template>
