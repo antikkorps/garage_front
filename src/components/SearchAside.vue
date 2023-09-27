0ada9e2d2d90f877525d93cc9a03816c21a7c04e
Franck <franckvienot7@gmail.com>
2023-09-27 23:27:28 +0200

    prepared live search params without reload

--- src/components/SearchAside.vue
+++ src/components/SearchAside.vue
@@ -1,3 +1,41 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+import axios from 'axios'
+import apiConfig from '@/config/apiConfig'
+
+const baseUrl = apiConfig.production.baseUrl
+const endpoint = apiConfig.production.endpoints.annoncesQuery
+const annonces = ref<{ id: number; title: string }[]>([])
+
+const searchQuery = ref('')
+const selectedBrands = ref([])
+const selectedKilometerRange = ref('')
+const selectedYear = ref('')
+
+const performSearch = async () => {
+  try {
+    const response = await axios.get(`${baseUrl}${endpoint}`, {
+      params: {
+        query: searchQuery.value,
+        brands: selectedBrands.value,
+        kilometerRange: selectedKilometerRange.value,
+        year: selectedYear.value
+      }
+    })
+
+    // Mettez à jour les annonces avec les résultats de la recherche
+    annonces.value = response.data
+  } catch (error) {
+    console.error('Erreur lors de la recherche :', error)
+  }
+}
+
+watch([searchQuery, selectedBrands, selectedKilometerRange, selectedYear], () => {
+  // Vous pouvez exécuter la recherche en utilisant les valeurs actuelles des filtres
+  performSearch()
+})
+</script>
+
 <template>
   <div class="grid-cols-1 sm:grid md:grid-cols-2 w-full sm:w-3/4 place-content-evenly">
     <!-- Champs de recherche -->
@@ -6,6 +44,7 @@
         type="text"
         class="w-full mx-auto px-4 py-2 text-gray-800 border border-gray-300 rounded-md focus:ring focus:ring-blue-200 inline-flex items-center"
         placeholder="Recherche rapide..."
+        v-model="searchQuery"
       />
     </div>
 
@@ -18,7 +57,7 @@
         <h4 class="text-sm font-medium">Marques</h4>
         <!-- Exemple de filtres de marques -->
         <label class="inline-flex items-center mt-2">
-          <input type="checkbox" class="text-blue-500 rounded" />
+          <input type="checkbox" class="text-blue-500 rounded" v-model="selectedBrands" />
           <span class="ml-2">Marque 1</span>
         </label>
         <label class="inline-flex items-center mt-2">
@@ -35,6 +74,7 @@
         <label class="block mt-2">
           <select
             class="w-full px-2 py-1 text-gray-800 border border-gray-300 rounded-md focus:ring focus:ring-blue-200"
+            v-model="selectedKilometerRange"
           >
             <option value="0-5000">0-5000 km</option>
             <option value="5000-10000">5000-10000 km</option>
@@ -51,6 +91,7 @@
         <label class="block mt-2">
           <select
             class="w-full px-2 py-1 text-gray-800 border border-gray-300 rounded-md focus:ring focus:ring-blue-200"
+            v-model="selectedYear"
           >
             <option value="2023">2023</option>
             <option value="2022">2022</option>
