bd22401ea55ccada1f143b8bc36b939e650feadc
Franck <franckvienot7@gmail.com>
2023-10-10 21:49:52 +0200

    still preparing annonce form to add annonce

--- src/components/AddAnnonceForm.vue
+++ src/components/AddAnnonceForm.vue
@@ -5,38 +5,12 @@ import { PhotoIcon } from '@heroicons/vue/24/solid'
 <template>
   <div class="grid mt-10">
     <form class="w-full sm:w-1/3 justify-self-center">
-      <div class="space-y-12">
-        <div class="border-b border-gray-900/10 pb-12">
+      <div class="space-y-5">
+        <div class="border-b border-gray-900/10 pb-5">
           <h2 class="text-base font-semibold leading-7 text-gray-900">Ajouter une annonce</h2>
           <p class="mt-1 text-sm leading-6 text-gray-600">
             Merci de remplir ce formulaire pour poster une nouvelle annonce.
           </p>
-
-          <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-            <div class="col-span-full">
-              <label for="cover-photo" class="block text-sm font-medium leading-6 text-gray-900"
-                >Cover photo</label
-              >
-              <div
-                class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10"
-              >
-                <div class="text-center">
-                  <PhotoIcon class="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
-                  <div class="mt-4 flex text-sm leading-6 text-gray-600">
-                    <label
-                      for="file-upload"
-                      class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
-                    >
-                      <span>Upload a file</span>
-                      <input id="file-upload" name="file-upload" type="file" class="sr-only" />
-                    </label>
-                    <p class="pl-1">or drag and drop</p>
-                  </div>
-                  <p class="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
 
         <div class="border-b border-gray-900/10 pb-12">
@@ -46,65 +20,108 @@ import { PhotoIcon } from '@heroicons/vue/24/solid'
           </p>
 
           <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-            <div class="sm:col-span-3">
-              <label for="first-name" class="block text-sm font-medium leading-6 text-gray-900"
-                >First name</label
+            <div class="col-span-full">
+              <label for="title" class="block text-sm font-medium leading-6 text-gray-900"
+                >Titre</label
               >
               <div class="mt-2">
                 <input
                   type="text"
-                  name="first-name"
-                  id="first-name"
-                  autocomplete="given-name"
+                  name="title"
+                  id="title"
+                  autocomplete="title"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div class="col-span-full">
+              <label for="description" class="block text-sm font-medium leading-6 text-gray-900"
+                >Description</label
+              >
+              <div class="mt-2">
+                <textarea
+                  id="description"
+                  name="description"
+                  rows="3"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                ></textarea>
+              </div>
+              <p class="mt-3 text-sm leading-6 text-gray-600">
+                Ecrivez la description de votre annonce ici.
+              </p>
+            </div>
+
+            <div class="sm:col-span-2 sm:col-start-1">
+              <label for="price" class="block text-sm font-medium leading-6 text-gray-900"
+                >Prix (Euros)</label
+              >
+              <div class="mt-2">
+                <input
+                  type="number"
+                  name="price"
+                  id="price"
+                  autocomplete="annonce-price"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div class="sm:col-span-2">
+              <label for="kilometrage" class="block text-sm font-medium leading-6 text-gray-900"
+                >Kilométrage</label
+              >
+              <div class="mt-2">
+                <input
+                  type="number"
+                  name="kilometrage"
+                  id="kilometrage"
+                  autocomplete="annonce-kilometrage"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div class="sm:col-span-2">
+              <label
+                for="yearofcirculation"
+                class="block text-sm font-medium leading-6 text-gray-900"
+                >Année</label
+              >
+              <div class="mt-2">
+                <input
+                  type="number"
+                  name="yearofcirculation"
+                  id="yearofcirculation"
+                  autocomplete="yearofcirculation"
                   class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
 
             <div class="sm:col-span-3">
-              <label for="last-name" class="block text-sm font-medium leading-6 text-gray-900"
-                >Last name</label
-              >
-              <div class="mt-2">
-                <input
-                  type="text"
-                  name="last-name"
-                  id="last-name"
-                  autocomplete="family-name"
-                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div class="sm:col-span-4">
-              <label for="email" class="block text-sm font-medium leading-6 text-gray-900"
-                >Email address</label
-              >
-              <div class="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autocomplete="email"
-                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div class="sm:col-span-3">
-              <label for="country" class="block text-sm font-medium leading-6 text-gray-900"
-                >Country</label
+              <label for="brand" class="block text-sm font-medium leading-6 text-gray-900"
+                >Marque</label
               >
               <div class="mt-2">
                 <select
-                  id="country"
-                  name="country"
-                  autocomplete="country-name"
+                  id="brand"
+                  name="brand"
+                  autocomplete="brand-name"
                   class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
                 >
-                  <option>United States</option>
-                  <option>Canada</option>
-                  <option>Mexico</option>
+                  <option>Choisissez une marque</option>
+                  <option>Toyota</option>
+                  <option>Opel</option>
+                  <option>Renault</option>
+                  <option>Peugeot</option>
+                  <option>Citroën</option>
+                  <option>Volkswagen</option>
+                  <option>Mercedes</option>
+                  <option>Hyundai</option>
+                  <option>BMW</option>
+                  <option>MG</option>
+                  <option>Fiat</option>
                 </select>
               </div>
             </div>
@@ -119,78 +136,59 @@ import { PhotoIcon } from '@heroicons/vue/24/solid'
                   name="street-address"
                   id="street-address"
                   autocomplete="street-address"
-                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div class="sm:col-span-2 sm:col-start-1">
-              <label for="city" class="block text-sm font-medium leading-6 text-gray-900"
-                >City</label
-              >
-              <div class="mt-2">
-                <input
-                  type="text"
-                  name="city"
-                  id="city"
-                  autocomplete="address-level2"
-                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div class="sm:col-span-2">
-              <label for="region" class="block text-sm font-medium leading-6 text-gray-900"
-                >State / Province</label
-              >
-              <div class="mt-2">
-                <input
-                  type="text"
-                  name="region"
-                  id="region"
-                  autocomplete="address-level1"
-                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div class="sm:col-span-2">
-              <label for="postal-code" class="block text-sm font-medium leading-6 text-gray-900"
-                >ZIP / Postal code</label
-              >
-              <div class="mt-2">
-                <input
-                  type="text"
-                  name="postal-code"
-                  id="postal-code"
-                  autocomplete="postal-code"
-                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
           </div>
         </div>
 
-        <div class="border-b border-gray-900/10 pb-12">
+        <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          <div class="col-span-full">
+            <label for="cover-photo" class="block text-sm font-medium leading-6 text-gray-900"
+              >Image mise en avant</label
+            >
+            <div
+              class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10"
+            >
+              <div class="text-center">
+                <PhotoIcon class="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
+                <div class="mt-4 flex text-sm leading-6 text-gray-600">
+                  <label
+                    for="file-upload"
+                    class="relative cursor-pointer rounded-md bg-white font-semibold text-red-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-red-600 focus-within:ring-offset-2 hover:text-red-500"
+                  >
+                    <span>Charger un fichier</span>
+                    <input id="file-upload" name="file-upload" type="file" class="sr-only" />
+                  </label>
+                  <p class="pl-1">ou glisser déposer</p>
+                </div>
+                <p class="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="border-b border-gray-900/10 pb-1">
           <h2 class="text-base font-semibold leading-7 text-gray-900">Publication</h2>
           <p class="mt-1 text-sm leading-6 text-gray-600">
-            Afin de choisir si vous souhaitez que votre annonce s'affiche sur le site ou non.
+            Ici vous définissez la publication et la mise en avant d'une annonce sur le site.
           </p>
 
-          <div class="mt-10 space-y-10">
+          <div class="mt-5 space-y-10">
             <fieldset>
               <div class="mt-6 space-y-6">
                 <div class="relative flex gap-x-3">
                   <div class="flex h-6 items-center">
                     <input
-                      id="comments"
-                      name="comments"
+                      id="published"
+                      name="published"
                       type="checkbox"
                       class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
                     />
                   </div>
                   <div class="text-sm leading-6">
-                    <label for="comments" class="font-medium text-gray-900">Publier</label>
+                    <label for="published" class="font-medium text-gray-900">Publier</label>
                     <p class="text-gray-500">
                       Si vous souhaitez que votre annonce s'affiche sur le site ou non.
                     </p>
@@ -199,14 +197,14 @@ import { PhotoIcon } from '@heroicons/vue/24/solid'
                 <div class="relative flex gap-x-3">
                   <div class="flex h-6 items-center">
                     <input
-                      id="candidates"
-                      name="candidates"
+                      id="featured"
+                      name="featured"
                       type="checkbox"
                       class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
                     />
                   </div>
                   <div class="text-sm leading-6">
-                    <label for="candidates" class="font-medium text-gray-900">Mise en avant</label>
+                    <label for="featured" class="font-medium text-gray-900">Mettre en avant</label>
                     <p class="text-gray-500">
                       Mettre l'annonce en avant sur le site avec le filtre "nos coups de coeur".
                     </p>
@@ -222,7 +220,7 @@ import { PhotoIcon } from '@heroicons/vue/24/solid'
         <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
         <button
           type="submit"
-          class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          class="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
         >
           Save
         </button>
