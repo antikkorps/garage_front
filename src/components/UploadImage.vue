16c5bc2263fe6eb099fcac0d6a3e4e9750c7828a
Franck <franckvienot7@gmail.com>
2023-10-19 15:54:32 +0200

    added file upload to the backend server, POST is receiving an error due to the fact some fields are missing

--- src/components/UploadImage.vue
+++ src/components/UploadImage.vue
@@ -1,47 +1,63 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import axios from 'axios'
 import apiConfig from '@/config/apiConfig'
 
 const baseUrl = apiConfig.production.baseUrl
-const imageUploadEndpoint = apiConfig.production.imageUploadEndpoint
+const imageUploadEndpoint = apiConfig.production.endpoints.imageUploadEndpoint
 
-const uploadImageUrl = `${baseUrl}${imageUploadEndpoint}`
-const image = ref(null)
+const url = baseUrl + imageUploadEndpoint
+console.log(url)
 
-const onImageChange = (event: Event) => {
-  const files = event.target.files
+const uploadImage = async (file: File) => {
+  const token = await localStorage.getItem('jwt_token')
 
-  if (!files || !Array.isArray(files) || files.length === 0) {
-    return
-  }
-
-  const file = files[0]
-  image.value = file
+  await axios.post(url, file, {
+    headers: { 'Content-Type': 'image', Authorization: `Bearer ${token}` }
+  })
 }
 
-const handleSubmit = async () => {
-  try {
-    const token = await localStorage.getItem('jwt_token')
-    const postedImage = image.value
+const handleFileUpload = (event: Event) => {
+  const input = event.target as HTMLInputElement
+  const file = input.files?.[0]
 
-    await axios.post(uploadImageUrl, postedImage, {
-      headers: {
-        Authorization: `Bearer ${token}`
-      },
-      data: {
-        image: postedImage
-      }
-    })
-  } catch (error) {
-    console.error(error)
+  if (file) {
+    uploadImage(file)
   }
 }
 </script>
 
 <template>
   <div>
-    <input type="file" name="image" v-on:change="onImageChange" />
-    <button class="buttonPrimary" type="submit" @click="handleSubmit">Envoyer</button>
+    <div class="flex items-center justify-center w-full">
+      <label
+        for="dropzone-file"
+        class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+      >
+        <div class="flex flex-col items-center justify-center pt-5 pb-6">
+          <svg
+            class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 16"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+            />
+          </svg>
+          <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
+            <span class="font-semibold">Cliquez pour choisir le fichier</span> ou glissez-le
+          </p>
+          <p class="text-xs text-gray-500 dark:text-gray-400">
+            SVG, PNG, JPG or GIF (MAX. 800x400px)
+          </p>
+        </div>
+        <input id="dropzone-file" type="file" class="hidden" @change="handleFileUpload" />
+      </label>
+    </div>
   </div>
 </template>
