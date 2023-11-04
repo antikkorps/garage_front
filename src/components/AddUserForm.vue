124b3b08ae6df74c3c3fbf6f4904dd37ca48b240
Franck <franckvienot7@gmail.com>
2023-11-04 23:09:16 +0100

    made some visual changes

--- src/components/AddUserForm.vue
+++ src/components/AddUserForm.vue
@@ -9,9 +9,31 @@ const endpoint = apiConfig.production.endpoints.signup
 const signupQuery = `${baseUrl}${endpoint}`
 
 const email = ref('')
+const emailError = ref('')
+
 const password = ref('')
 const passwordConfirmation = ref('')
+const passwordError = ref('')
 const confirmationMessage = ref('')
+
+const validateEmail = () => {
+  const regex = /\S+@\S+\.\S+/
+  if (!regex.test(email.value)) {
+    emailError.value = "l'email doit être un email valide"
+  } else {
+    emailError.value = ''
+  }
+}
+
+const validatePassword = () => {
+  const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/
+  if (!regex.test(password.value)) {
+    passwordError.value =
+      'Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule et un chiffre'
+  } else {
+    passwordError.value = ''
+  }
+}
 
 const addUser = () => {
   if (password.value !== passwordConfirmation.value) {
@@ -59,24 +81,29 @@ const confirmationMessageClass = computed(() => {
 
           <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div class="sm:col-span-4">
-              <label for="username" class="block text-sm font-medium leading-6 text-gray-900"
-                >Username</label
+              <label for="email" class="block text-sm font-medium leading-6 text-gray-900"
+                >Email</label
               >
               <div class="mt-2">
                 <div
                   class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
                 >
                   <input
-                    type="text"
-                    name="username"
-                    id="username"
-                    autocomplete="username"
+                    type="email"
+                    name="email"
+                    id="email"
+                    autocomplete="email"
                     class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                     placeholder="john.doe@gmail.com"
                     v-model="email"
+                    @input="validateEmail"
                   />
                 </div>
               </div>
+
+              <p v-if="emailError" class="text-red-600 text-xs">
+                {{ emailError }}
+              </p>
             </div>
           </div>
 
@@ -97,8 +124,14 @@ const confirmationMessageClass = computed(() => {
                     class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                     placeholder="Saisissez le mot de passe"
                     v-model="password"
+                    @input="validatePassword"
                   />
                 </div>
+              </div>
+              <div class="grid-cols-1 gap-x-6">
+                <p v-if="passwordError" class="text-red-600 text-xs text-justify">
+                  {{ passwordError }}
+                </p>
               </div>
             </div>
           </div>
