b0827473c773befa69dacb2db2307306b378ccb0
Franck <franckvienot7@gmail.com>
2023-11-20 21:56:21 +0100

    added services to the services section

--- src/views/ServicesView.vue
+++ src/views/ServicesView.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import NavbarMain from '@/components/NavbarMain.vue'
+import OurServices from '@/components/OurServices.vue'
 import FooterSection from '@/components/FooterSection.vue'
 
 const currentPage = 'services'
 </script>
 <template>
   <NavbarMain :current-page="currentPage" />
-  <div>
-    <h2 class="title_page">Service du garage</h2>
+  <div class="title_page">Nos services</div>
+  <div class="main_content">
+    <OurServices :current-page="currentPage" />
   </div>
   <FooterSection />
 </template>
