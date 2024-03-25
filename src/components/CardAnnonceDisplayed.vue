ab32a762a19d4100893f725cd973311e0bb9166e
Franck <franckvienot7@gmail.com>
2024-03-25 17:40:30 +0100

    modify the position of the Annonce à laffiche

--- src/components/CardAnnonceDisplayed.vue
+++ src/components/CardAnnonceDisplayed.vue
@@ -7,7 +7,7 @@ const { annonce } = defineProps(['annonce'])
   <div
     class="relative overflow-hidden img_container mx-3 mt-6 flex flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-gray-900 sm:shrink-0 sm:grow sm:basis-0"
   >
-    <div class="absolute top-0 left-0 transform -rotate-33 bg-yellow-400 p-4 rounded-md shadow-md">
+    <div class="absolute bottom-0 left-0 transform bg-yellow-400 p-2 rounded-md shadow-md">
       <p class="text-sm font-semibold text-gray-800">📢 Annonce à l'affiche</p>
     </div>
     <RouterLink :to="`/annonces/${annonce.id}`">
