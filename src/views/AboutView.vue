25da6162035af6b4c652926fd2e80bf52cdba1bd
Franck <franckvienot7@gmail.com>
2024-03-27 14:51:58 +0100

    correct display in about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -5,27 +5,29 @@ import FooterSection from '@/components/FooterSection.vue'
 const currentPage = 'about'
 </script>
 <template>
-  <NavbarMain :current-page="currentPage" />
-  <div class="about dark:bg-gray-900 dark:text-white">
-    <div class="sm:w-1/2 flex flex-col justify-center mx-3 px-3 sm:mx-auto">
-      <h1 class="title_page">A propos du garage</h1>
-      <div class="presentation my-10">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam voluptatibus, quibusdam,
-          quia, quae voluptatem voluptatum quos voluptates quod doloribus quas natus. Quisquam
-          voluptatibus, quibusdam, quia, quae voluptatem voluptatum quos voluptates quod doloribus
-          quas natus.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam voluptatibus, quibusdam,
-          quia, quae voluptatem voluptatum quos voluptates quod doloribus quas natus. Quisquam
-          voluptatibus, quibusdam, quia, quae voluptatem voluptatum quos voluptates quod doloribus
-          quas natus.
-        </p>
+  <div class="h-screen">
+    <NavbarMain :current-page="currentPage" />
+    <div class="about dark:bg-gray-900 dark:text-white">
+      <div class="sm:w-1/2 flex flex-col justify-center mx-3 px-3 sm:mx-auto">
+        <h1 class="title_page">A propos du garage</h1>
+        <div class="presentation my-10">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam voluptatibus,
+            quibusdam, quia, quae voluptatem voluptatum quos voluptates quod doloribus quas natus.
+            Quisquam voluptatibus, quibusdam, quia, quae voluptatem voluptatum quos voluptates quod
+            doloribus quas natus.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam voluptatibus,
+            quibusdam, quia, quae voluptatem voluptatum quos voluptates quod doloribus quas natus.
+            Quisquam voluptatibus, quibusdam, quia, quae voluptatem voluptatum quos voluptates quod
+            doloribus quas natus.
+          </p>
+        </div>
       </div>
     </div>
+    <FooterSection />
   </div>
-  <FooterSection />
 </template>
 
 <style scoped></style>
