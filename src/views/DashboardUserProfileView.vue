ec7d7bba48f0649b57886ae9a237b0027c24603c
Franck <franckvienot7@gmail.com>
2024-04-01 23:17:40 +0200

    modify text in user Profile

--- src/views/DashboardUserProfileView.vue
+++ src/views/DashboardUserProfileView.vue
@@ -59,7 +59,7 @@ onMounted(() => {
           <div class="px-4 py-5 sm:px-6">
             <h3 class="text-lg leading-6 font-medium text-gray-900">Profil Utilisateur</h3>
             <p class="mt-1 max-w-2xl text-sm text-gray-500">
-              Details et informations à propos de l'utilisateur.
+              Bienvenue {{ userProfile.firstName }} sur votre profil !
             </p>
           </div>
           <div class="border-t border-gray-200">
